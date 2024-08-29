a61ba5faad62775e3e6d27a78f42a38adbbb5dc0
Michael Bilevich <mickelbil84@gmail.com>
2024-08-29 23:05:54 +0300

    Draw test icons quickly

--- screen_test.py
+++ screen_test.py
@@ -1,5 +1,6 @@
 import time
 
+from icons import *
 from DWIN_Screen import T5UIC1_LCD
 
 LCD_COM_Port = '/dev/ttyAML6'
@@ -7,6 +8,8 @@ LCD_COM_Port = '/dev/ttyAML6'
 if __name__ == "__main__":
     lcd = T5UIC1_LCD(LCD_COM_Port)
 
+    frame = 0
+    icon_id = 0
     while True:
         # Get date time in format: "YYYY-MM-DD HH:MM:SS"
         date_time = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -14,6 +17,14 @@ if __name__ == "__main__":
         # Clear screen
         lcd.Frame_Clear(lcd.Color_Bg_Black)
         lcd.Draw_String(True, False, lcd.font14x28, lcd.Color_White, 0, 10, 10, date_time)
+        lcd.ICON_Show(ICON_LIB, icon_id, 10, 50)
         lcd.UpdateLCD()
+
+        frame += 1
+        if frame >= 60 / 4:
+            frame = 0
+            icon_id += 1
+            if icon_id > 91:
+                icon_id = 0
 
         time.sleep(1 / 60)
