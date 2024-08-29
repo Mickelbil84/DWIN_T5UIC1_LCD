5ff7316b28b7965f63c08780eaf4b89345e97d13
Michael <mickelbil84@gmail.com>
2024-08-29 22:10:15 +0100

    Re-add basic main menu functionality

--- screen_test.py
+++ screen_test.py
@@ -1,32 +1,66 @@
 import time
 
+# from RPi import GPIO
+import LePotatoPi.GPIO.GPIO as GPIO
+
 from icons import *
+from screens import *
+from encoder import Encoder
 from DWIN_Screen import T5UIC1_LCD
 
 LCD_COM_Port = '/dev/ttyAML6'
 
 FPS = 5
 
+def current_milli_time():
+	return round(time.time() * 1000)
+
+class ScreenTest(object):
+    ENCODER_DIFF_NO = 0  # no state
+    ENCODER_DIFF_CW = 1  # clockwise rotation
+    ENCODER_DIFF_CCW = 2  # counterclockwise rotation
+    ENCODER_DIFF_ENTER = 3   # click
+    ENCODER_WAIT = 180
+    ENCODER_WAIT_ENTER = 300
+    EncoderRateLimit = True
+         
+    def __init__(self):
+        self.lcd = T5UIC1_LCD(LCD_COM_Port)
+        self.screen = Screen_MainMenu(self.lcd)
+
+        self.encoder = Encoder(26, 19)
+        self.EncodeLast = 0
+        self.EncodeMS = current_milli_time() + self.ENCODER_WAIT
+        self.EncodeEnter = current_milli_time() + self.ENCODER_WAIT_ENTER
+        self.encoder.callback = self.encoder_callback
+
+    def get_encoder_state(self):
+        if self.EncoderRateLimit:
+            if self.EncodeMS > current_milli_time():
+                return self.ENCODER_DIFF_NO
+            self.EncodeMS = current_milli_time() + self.ENCODER_WAIT
+
+        if self.encoder.value < self.EncodeLast:
+            self.EncodeLast = self.encoder.value
+            return self.ENCODER_DIFF_CW
+        elif self.encoder.value > self.EncodeLast:
+            self.EncodeLast = self.encoder.value
+            return self.ENCODER_DIFF_CCW
+        # elif not GPIO.input(self.button_pin):
+        #     if self.EncodeEnter > current_milli_time(): # prevent double clicks
+        #         return self.ENCODER_DIFF_NO
+        #     self.EncodeEnter = current_milli_time() + self.ENCODER_WAIT_ENTER
+        #     return self.ENCODER_DIFF_ENTER
+        else:
+            return self.ENCODER_DIFF_NO
+        
+    def encoder_callback(self):
+        state = self.get_encoder_state()
+        if not self.screen.busy:
+            self.screen.handle_input(state)
+
 if __name__ == "__main__":
-    lcd = T5UIC1_LCD(LCD_COM_Port)
+    ScreenTest()
 
-    frame = 0
-    icon_id = 0
     while True:
-        # Get date time in format: "YYYY-MM-DD HH:MM:SS"
-        date_time = time.strftime("%Y-%m-%d %H:%M:%S")
-
-        # Clear screen
-        lcd.Frame_Clear(lcd.Color_Bg_Black)
-        lcd.Draw_String(True, False, lcd.font14x28, lcd.Color_White, 0, 10, 10, date_time)
-        lcd.ICON_Show(ICON_LIB, icon_id, 10, 50)
-        lcd.UpdateLCD()
-
-        frame += 1
-        if frame >= 60 / 4:
-            frame = 0
-            icon_id += 1
-            if icon_id > 91:
-                icon_id = 0
-
-        time.sleep(1 / FPS)
+        pass
--- screens/screen_main_menu.py
+++ screens/screen_main_menu.py
@@ -1,11 +1,83 @@
+import time
+
 from icons import *
 from DWIN_Screen import T5UIC1_LCD
+from screens.screen_base import Screen_Base, BUSY_DELAY
 
-class Screen_MainMenu(object):
+class Screen_MainMenu(Screen_Base):
     def __init__(self, lcd: T5UIC1_LCD):
-        self.lcd = lcd
+        self.activeIcon = "print"
+        self.icons = ["print", "prepare", "control", "info"]
+
+        Screen_Base.__init__(self, lcd, "Home")
+    
+    def init(self):
+        self._icon_print()
+        self._icon_prepare()
+        self._icon_control()
+        self._icon_info()
+
+    def handle_input(self, state):
+        if state == 1:
+            self._change_active_icon(1)
+        if state == 2:
+            self._change_active_icon(-1)
+
+    # d is +-1 (direction of change)
+    def _change_active_icon(self, d: int):
+        print("change", d)
+        self.busy = True
+        curr = self.icons.index(self.activeIcon)
+        idx = (curr + d) % len(self.icons)
+        self.activeIcon = self.icons[idx]
+
+        self.init() # Redraw
+        self.refresh()
         
+        time.sleep(BUSY_DELAY)
+        self.busy = False
 
+    def _icon_print(self):
+        if self.activeIcon == "print":
+            self.lcd.ICON_Show(ICON_LIB, ICON_Print_1, 17, 130)
+            self.lcd.Draw_Rectangle(0, self.lcd.Color_White, 17, 130, 126, 229)
+            self.lcd.Frame_AreaCopy(1, 1, 451, 31, 463, 57, 201)
+        else:
+            self.lcd.ICON_Show(ICON_LIB, ICON_Print_0, 17, 130)
+            self.lcd.Frame_AreaCopy(1, 1, 423, 31, 435, 57, 201)
 
-    def handle_input(self):
-        pass
+    def _icon_prepare(self):
+        if self.activeIcon == "prepare":
+            self.lcd.ICON_Show(ICON_LIB, ICON_Prepare_1, 145, 130)
+            self.lcd.Draw_Rectangle(0, self.lcd.Color_White, 145, 130, 254, 229)
+            self.lcd.Frame_AreaCopy(1, 33, 451, 82, 466, 175, 201)
+        else:
+            self.lcd.ICON_Show(ICON_LIB, ICON_Prepare_0, 145, 130)
+            self.lcd.Frame_AreaCopy(1, 33, 423, 82, 438, 175, 201)
+
+    def _icon_control(self):
+        if self.activeIcon == "control":
+            self.lcd.ICON_Show(ICON_LIB, ICON_Control_1, 17, 246)            
+            self.lcd.Draw_Rectangle(0, self.lcd.Color_White, 17, 246, 126, 345)
+            self.lcd.Frame_AreaCopy(1, 85, 451, 132, 463, 48, 318)
+        else:
+            self.lcd.ICON_Show(ICON_LIB, ICON_Control_0, 17, 246)
+            self.lcd.Frame_AreaCopy(1, 85, 423, 132, 434, 48, 318)
+
+    def _icon_leveling(self):
+        if self.activeIcon == "leveling":
+            self.lcd.ICON_Show(ICON_LIB, ICON_Leveling_1, 145, 246)
+            self.lcd.Draw_Rectangle(0, self.lcd.Color_White, 145, 246, 254, 345)
+            self.lcd.Frame_AreaCopy(1, 84, 437, 120, 449, 182, 318)
+        else:
+            self.lcd.ICON_Show(ICON_LIB, ICON_Leveling_0, 145, 246)
+            self.lcd.Frame_AreaCopy(1, 84, 465, 120, 478, 182, 318)
+
+    def _icon_info(self):
+        if self.activeIcon == "info":
+            self.lcd.ICON_Show(ICON_LIB, ICON_Info_1, 145, 246)
+            self.lcd.Draw_Rectangle(0, self.lcd.Color_White, 145, 246, 254, 345)
+            self.lcd.Frame_AreaCopy(1, 132, 451, 159, 466, 186, 318)
+        else:
+            self.lcd.ICON_Show(ICON_LIB, ICON_Info_0, 145, 246)
+            self.lcd.Frame_AreaCopy(1, 132, 423, 159, 435, 186, 318)
