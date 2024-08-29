50353da263417fd7b0fbb575ff3f3e1547115416
Michael Bilevich <mickelbil84@gmail.com>
2024-08-29 23:13:41 +0300

    Start screens refactor

---
+++ screens/screen_main_menu.py
@@ -0,0 +1,11 @@
+from icons import *
+from DWIN_Screen import T5UIC1_LCD
+
+class Screen_MainMenu(object):
+    def __init__(self, lcd: T5UIC1_LCD):
+        self.lcd = lcd
+        
+
+
+    def handle_input(self):
+        pass
