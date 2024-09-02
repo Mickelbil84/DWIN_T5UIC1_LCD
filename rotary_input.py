2777c881e3cb28f4a0e73900784e54373a9f5122
Michael <mickelbil84@gmail.com>
2024-09-02 20:44:40 +0100

    More changes

--- rotary_input.py
+++ rotary_input.py
@@ -1,5 +1,6 @@
 # Class to monitor a rotary encoder and update a value.  You can either read the value when you need it, by calling getValue(), or
 # you can configure a callback which will be called whenever the value changes.
+# Adapted from: https://github.com/nstansby/rpi-rotary-encoder-python/blob/master/encoder.py
 
 import time
 
@@ -8,61 +9,80 @@ import LePotatoPi.GPIO.GPIO as GPIO
 
 from events import *
 
-ROTARY_DEBOUNCE = 0.002
+ROTARY_DEBOUNCE = 0
 
 class RotaryInput:
-    def __init__(self, left_pin, right_pin, button_pin):
-        self.left_pin, self.right_pin, self.button_pin = left_pin, right_pin, button_pin
+    def __init__(self, left_pin, right_pin, button_pin, beep_pin):
+        self.left_pin, self.right_pin, self.button_pin, self.beep_pin = left_pin, right_pin, button_pin, beep_pin
 
         GPIO.setup(self.left_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.right_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.beep_pin, GPIO.OUT)
 
         GPIO.add_event_detect(self.left_pin, GPIO.BOTH, callback=self.update_state)  
         GPIO.add_event_detect(self.right_pin, GPIO.BOTH, callback=self.update_state)  
 
-        self.last_state = (GPIO.input(self.left_pin) << 1) | GPIO.input(self.right_pin)
-        self.last_time = time.time()
+        self.value = 0
+        self.state = '00'
+        self.direction = None
         self.events = []
+        self.callback = None
     
-    def update_state(self):
-        if time.time() - self.last_time < ROTARY_DEBOUNCE:
-            return
-        
-        current_state = (GPIO.input(self.left_pin) << 1) | GPIO.input(self.right_pin)
-        if current_state == self.last_state:
-            return
+    def update_state(self):        
+        p1 = GPIO.input(self.left_pin)
+        p2 = GPIO.input(self.right_pin)
+        newState = "{}{}".format(p1, p2)
+        if self.state == "00": # Resting position
+            if newState == "01": # Turned right 1
+                self.direction = "R"
+            elif newState == "10": # Turned left 1
+                self.direction = "L"
 
-        if (self.last_state == 0b00 and current_state == 0b01) or \
-            (self.last_state == 0b01 and current_state == 0b11) or \
-            (self.last_state == 0b11 and current_state == 0b10) or \
-            (self.last_state == 0b10 and current_state == 0b00):
-            self.events.append(EVENT_ROTARY_CCW)
-        elif (self.last_state == 0b00 and current_state == 0b10) or \
-            (self.last_state == 0b10 and current_state == 0b11) or \
-            (self.last_state == 0b11 and current_state == 0b01) or \
-            (self.last_state == 0b01 and current_state == 0b00):
-            self.events.append(EVENT_ROTARY_CW)
-        
-        self.last_state = current_state
-        self.last_time = time.time()
+        elif self.state == "01": # R1 or L3 position
+            if newState == "11": # Turned right 1
+                self.direction = "R"
+            elif newState == "00": # Turned left 1
+                if self.direction == "L":
+                    self.value = self.value - 1
+                    if self.callback is not None:
+                        self.events.append(EVENT_ROTARY_CW if self.direction == "L" else EVENT_ROTARY_CCW)
 
+        elif self.state == "10": # R3 or L1
+            if newState == "11": # Turned left 1
+                self.direction = "L"
+            elif newState == "00": # Turned right 1
+                if self.direction == "R":
+                    self.value = self.value + 1
+                    if self.callback is not None:
+                        self.events.append(EVENT_ROTARY_CW if self.direction == "L" else EVENT_ROTARY_CCW)
+
+        else: # self.state == "11"
+            if newState == "01": # Turned left 1
+                self.direction = "L"
+            elif newState == "10": # Turned right 1
+                self.direction = "R"
+            elif newState == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
+                if self.direction == "L":
+                    self.value = self.value - 1
+                    if self.callback is not None:
+                        self.events.append(EVENT_ROTARY_CW if self.direction == "L" else EVENT_ROTARY_CCW)
+                elif self.direction == "R":
+                    self.value = self.value + 1
+                    if self.callback is not None:
+                        self.events.append(EVENT_ROTARY_CW if self.direction == "L" else EVENT_ROTARY_CCW)
+                
+        self.state = newState
+        
     def poll_event(self) -> int:
         if len(self.events) == 0:
             return EVENT_ROTARY_NONE
         
-        # Cast majority vote on events
-        cnts = {}
-        for event in self.events:
-            if event not in cnts:
-                cnts[event] = 0
-            cnts[event] += 1
-        best_event = None
-        best_cnt = -1
-        for event, cnt in cnts.items():
-            if cnt > best_cnt:
-                best_cnt = cnt
-                best_event = event
-
+        e = self.events[-1]
         self.events = []
-        return best_event
+        return e
+    
+    def beep(self):
+        GPIO.output(self.beep_pin, 1)
+        time.sleep(0.1)
+        GPIO.output(self.beep_pin, 0)
--- screen_test.py
+++ screen_test.py
@@ -11,6 +11,7 @@ from DWIN_Screen import T5UIC1_LCD
 
 LCD_COM_Port = '/dev/ttyAML6'
 FPS = 60
+ROTARY_DELAY = 0.1
 
 def current_milli_time():
 	return round(time.time() * 1000)
@@ -19,14 +20,25 @@ class ScreenTest(object):
     def __init__(self):
         self.lcd = T5UIC1_LCD(LCD_COM_Port)
         self.screen = Screen_MainMenu(self.lcd)
-        self.rotary = RotaryInput(26, 19 , 13)
+        self.rotary = RotaryInput(26, 19 , 13, 6)
+        # self.rotary.callback = self.rotary_callback
+        self.rotary.beep()
+
+        self.last_rotary_time = time.time()
+
+    def handle_input(self, event):
+        if time.time() - self.last_rotary_time < ROTARY_DELAY:
+            return
+        self.screen.handle_input(event)
+        self.last_rotary_time = time.time()
 
     def run(self):
         while True:
             event = self.rotary.poll_event()
-            if event > 0:
+            if event >= 0:
+                self.rotary.beep()
                 print(event)
-            self.screen.handle_input(event)
+            self.handle_input(event)
             time.sleep(1 / FPS)
 
 if __name__ == "__main__":
